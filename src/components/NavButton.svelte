4b7f17cc32160f9657ffacae7af5bbc21480d46c
Ryan Pederson <ryanpedersonphotography@users.noreply.github.com>
2026-01-27 21:43:08 -0600

    feat: Improve nav label readability (stroke + shadow variants)

---
+++ src/components/NavButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import styles from "./NavButton.module.css";
+
+  export let label: string;
+  export let href: string;
+  export let variant: "light" | "dark" = "dark";
+  export let backgroundImage: string = "";
+
+  const style: string = backgroundImage
+    ? `background-image: url(${backgroundImage})`
+    : "";
+</script>
+
+<a
+  {href}
+  class="{styles.btn} {variant === 'light' ? styles.light : styles.dark}"
+  {style}
+  aria-label={label}
+>
+  <span class={styles.label}>{label}</span>
+</a>
